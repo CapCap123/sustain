d73ddad4c30dd6579806c813cfc81df84d8ba452
CapCap123 <50552769+CapCap123@users.noreply.github.com>
2020-06-04 16:46:45 -0400

    websiteName

--- scripts/background.js
+++ scripts/background.js
@@ -43,11 +43,7 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     chrome.tabs.query({active: true, currentWindow: true}, async function(tabs) {
       var currentTab = tabs[0]
       var currentURL = currentTab.url;
-      var urlArray = currentURL.split('/');
-      let name = urlArray[2];
-      let website = findWebsiteName(name);
-      var websiteArray = website.split(".")
-      let websiteName = websiteArray[0];
+      const websiteName = findWebsiteName(currentURL);
 
       chrome.storage.sync.get(websiteName, async function(result) {
         var results = result[websiteName];
@@ -72,155 +68,135 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   }
 });
 
-    function findWebsiteName(name) {
-      const www = "www."
-      var website = name;
-      if (name.includes(www) == true) {
-        website = name.replace(www,"");
-        } else {
-        website = name 
-        }
-    return website;
+function findWebsiteName(currentURL) {
+  var urlArray = currentURL.split('/');
+  let name = urlArray[2];
+  var website = name;
+  if (name.includes("www.") == true) {
+    website = name.replace("www.","");
+  }
+  var websiteFullArray = website.split(".")
+  console.log('website full array: ' + websiteFullArray)
+  var websiteArray = [websiteFullArray[0], websiteFullArray[1]]
+  console.log('website array: ' + websiteArray)
+  const websiteName = websiteArray.join('.');
+  console.log('website name: ' + websiteName)
+
+  
+return websiteName;
+}
+
+function setBadge(results) {
+  let colors = {
+    "red": '#cc0000',
+    "yellow":'#ecd23e',
+    "green": '#3a9337',
+    "grey": '#8a8c92'};
+
+  if (results.hasBusiness_ref == true) {
+    if (results.hasEsg == false) {
+      let badgeColor = colors.red;
+      return badgeColor
+    } else {
+      if (results.yahoo_percentile > 50) {
+        let badgeColor = colors.yellow;
+        return badgeColor
+      } else {
+        let badgeColor = colors.green;
+        return badgeColor
+      }
+    }
+  } else {
+    let badgeColor = colors.grey;      
+    return badgeColor  
   }
 
-    async function checkBrandName(websiteName){
-      try {
-      var brand = {};
-      brand.name = websiteName;
-      brand.website = websiteName;
-  
-      let results = await checkBusinessData(brand);
-    
-      return results
-    
-      } catch(error) {
-        console.log(error);
-      }
-    };
+}
 
-    function setBadge(results) {
-      let colors = {
-        "red": '#cc0000',
-        "yellow":'#ecd23e',
-        "green": '#3a9337',
-        "grey": '#8a8c92'};
+async function checkBrandName(websiteName){
+  try {
+  var brand = {};
+  brand.name = websiteName;
+  brand.website = websiteName;
 
-      if (results.hasBusiness_ref == true) {
-        if (results.hasEsg == false) {
-          let badgeColor = colors.red;
-          return badgeColor
-        } else {
-          if (results.yahoo_percentile > 50) {
-            let badgeColor = colors.yellow;
-            return badgeColor
-          } else {
-            let badgeColor = colors.green;
-            return badgeColor
-          }
-        }
-      } else {
-        let badgeColor = colors.grey;      
-        return badgeColor  
-      }
+  let results = await checkBusinessData(brand);
 
-    }
+  return results
 
-    async function checkBusinessData(brand) {
-      try {   
-        var matchedBrand = await checkBrand(brand);
-        var scrapedBusiness = await matchedBrand;
-        var yahooCode = await matchedBrand.business_ref;
-        console.log('yahoo code is ' + yahooCode)
-        if (!yahooCode || yahooCode.length < 1) {
-          console.log('no business ref found for this brand')
-          scrapedBusiness.new_business = false
-          scrapedBusiness.hasBusiness_ref = false
-          scrapedBusiness.hasEsg = false
-        } else {
-          matchedBrand.hasBusiness_ref = true
-          let businessQuery = db.collection('businesses').where('yahoo_uid', '==',  yahooCode)
-          let businessSnapshot = await businessQuery.get()          
-          if (businessSnapshot.empty) {
-            console.log('scraper for business needed')
-          } else { 
-            businessSnapshot.forEach(doc => {
-              scrapedBusiness = doc.data();
-              scrapedBusiness.new_business = false;
-              scrapedBusiness.business_ref = scrapedBusiness.yahoo_uid;
-              scrapedBusiness.business_name = scrapedBusiness.name;
-              scrapedBusiness.name = matchedBrand.name;
-              scrapedBusiness.hasBusiness_ref = true;
-              var esg = scrapedBusiness.yahoo_esg;
-                if ((!esg) || (esg.length < 1)) {
-                  scrapedBusiness.hasEsg = false
-                } else {
-                  scrapedBusiness.hasEsg = true
-                }
-            })
-          }
-        }
-      } catch(error) {
-        console.log(error);
-      }
-  
-      return scrapedBusiness
-    }
+  } catch(error) {
+    console.log(error);
+  }
+};
 
-    async function checkBrand(brand) {
-      try {
-      let websiteName = brand.website;
-        let brandQuery = db.collection('brands').where('websites', 'array-contains',  websiteName);
-        let querySnapshot = await brandQuery.get();
-        if (querySnapshot.empty) {
-          console.log('brand does not exist in db');
-          brand.new_brand = true;        
-        } else {
-          brand.new_brand = false;     
-          querySnapshot.forEach(doc => { //if brand exists
-            var businessRef = doc.data().business_ref
-            var businessName = doc.data().business_name
-            if((!businessRef) || businessRef.empty) {
-              console.log('this brand has no business ref')
-              brand.hasEsg = false;
-              brand.business_ref = ""      
-            } else{
-              brand.business_ref = businessRef
-              brand.business_name = businessName
-              brand.hasBusiness_ref == true
-              console.log('brand exists and has business ref')
+async function checkBusinessData(brand) {
+  try {   
+    var matchedBrand = await checkBrand(brand);
+    var scrapedBusiness = await matchedBrand;
+    var yahooCode = await matchedBrand.business_ref;
+    console.log('yahoo code is ' + yahooCode)
+    if (!yahooCode || yahooCode.length < 1) {
+      console.log('no business ref found for this brand')
+      scrapedBusiness.new_business = false
+      scrapedBusiness.hasBusiness_ref = false
+      scrapedBusiness.hasEsg = false
+    } else {
+      matchedBrand.hasBusiness_ref = true
+      let businessQuery = db.collection('businesses').where('yahoo_uid', '==',  yahooCode)
+      let businessSnapshot = await businessQuery.get()          
+      if (businessSnapshot.empty) {
+        console.log('scraper for business needed')
+      } else { 
+        businessSnapshot.forEach(doc => {
+          scrapedBusiness = doc.data();
+          scrapedBusiness.new_business = false;
+          scrapedBusiness.business_ref = scrapedBusiness.yahoo_uid;
+          scrapedBusiness.business_name = scrapedBusiness.name;
+          scrapedBusiness.name = matchedBrand.name;
+          scrapedBusiness.hasBusiness_ref = true;
+          var esg = scrapedBusiness.yahoo_esg;
+            if ((!esg) || (esg.length < 1)) {
+              scrapedBusiness.hasEsg = false
+            } else {
+              scrapedBusiness.hasEsg = true
             }
-          })
-        }
-      } catch(error) {
-        console.log(error)
+        })
       }
-  
-    return brand;
     }
+  } catch(error) {
+    console.log(error);
+  }
 
-    function saveBrand (brand) {
-      let brandRef = db.collection('brands');
-      let parent = brand.business_ref;
-      console.log('brand to be added to db' + JSON.stringify(brand));
-      if (!parent || parent == '') {
-        let addBrand = brandRef.add({
-          name: brand.name,
-          small_business: 'new',
-          websites: [brand.website],
-        }).then(ref => {
-          console.log('Added brand with ID: ', ref.id);
-        });
-        return addBrand;
-      } else {
-        let addBrand = brandRef.add({
-          name: brand.name,
-          small_business: 'new',
-          business_name: brand.business_name,
-          business_ref: brand.business_ref,
-          websites: [brand.website],
-        }).then(ref => {
-          console.log('Added brand with ID: ', ref.id);
-        });
-        return addBrand;
-      }
+  return scrapedBusiness
+}
+
+async function checkBrand(brand) {
+  try {
+  let websiteName = brand.website;
+    let brandQuery = db.collection('brands').where('websites', 'array-contains',  websiteName);
+    let querySnapshot = await brandQuery.get();
+    if (querySnapshot.empty) {
+      console.log('brand does not exist in db');
+      brand.new_brand = true;        
+    } else {
+      brand.new_brand = false;     
+      querySnapshot.forEach(doc => { //if brand exists
+        var businessRef = doc.data().business_ref
+        var businessName = doc.data().business_name
+        if((!businessRef) || businessRef.empty) {
+          console.log('this brand has no business ref')
+          brand.hasEsg = false;
+          brand.business_ref = ""      
+        } else{
+          brand.business_ref = businessRef
+          brand.business_name = businessName
+          brand.hasBusiness_ref == true
+          console.log('brand exists and has business ref')
+        }
+      })
     }
+  } catch(error) {
+    console.log(error)
+  }
+
+return brand;
+}
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,63 +1,61 @@
 import regeneratorRuntime from 'regenerator-runtime/runtime';
 
-  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-    var currentTab = tabs[0]
-    var currentURL = currentTab.url;
-    var urlArray = currentURL.split('/');
-    let name = urlArray[2];
-    let website = findWebsiteName(name);
+chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+  var currentTab = tabs[0]
+  var currentURL = currentTab.url;
+  let websiteName = findWebsiteName(currentURL);
 
-    var websiteArray = website.split(".")
-    let websiteName = websiteArray[0];
+  chrome.storage.sync.get(websiteName, function(result) {
+    console.log( websiteName + " results retrieved from storage in popup is " + result[websiteName]);
+    let answer = sendAnswer(result[websiteName]); 
+    document.getElementById("postEsgResults").innerHTML = answer;
+  })
+});
 
-    chrome.storage.sync.get(websiteName, function(result) {
-      console.log( websiteName + " results retrieved from storage in popup is " + result[websiteName]);
-      let answer = sendAnswer(result[websiteName]); 
-      document.getElementById("postEsgResults").innerHTML = answer;
-    })
-  });
-
-  function findWebsiteName(name) {
-    const www = "www."
-    var website = name;
-    if (name.includes(www) == true) {
-      website = name.replace(www,"");
-      } else {
-      website = name 
-      }
-  return website;
+function findWebsiteName(currentURL) {
+  var urlArray = currentURL.split('/');
+  let name = urlArray[2];
+  var website = name;
+  if (name.includes("www.") == true) {
+    website = name.replace("www.","");
   }
-
-function sendAnswer(results) {  
-  if(results.hasBusiness_ref == false) {
-    let answer =  (
-      "We did not find official information about "+ results.name
-      ); 
-      detailsButton.style.display = "none"
-      return answer
-  } else {
-    if (results.hasEsg == false) {
-      let answer = (
-      "This website belongs to "+ results.business_name + 
-      "<br>" + results.business_name + " did not make their information public"
-      );
-      detailsButton.style.display = "block"
-      return answer
-    } else if (results.hasEsg == true) {
-      let answer = (
-      "This website belongs to " + results.business_name +
-      ":<br>\u2022 ESG risk score: "+ results.yahoo_esg + "% (" + results.yahoo_percentile +
-      ")<br>\u2022 Environmental risk: "+ results.yahoo_envrisk
-      );
-      detailsButton.style.display = "block"
-      return answer
-    }
-  }
+  var websiteFullArray = website.split(".")
+  var websiteArray = [websiteFullArray[0], websiteFullArray[1]]
+  const websiteName = websiteArray.join('.');
+  
+return websiteName;
 }
 
-  var detailsButton = document.getElementById('details');
-  detailsButton.addEventListener('click', function() {
-    chrome.tabs.getSelected(null, function(tab) {      
-      alert('thats not ready yet');
-      });
-    }, false);
+function sendAnswer(results) {  
+if(results.hasBusiness_ref == false) {
+  let answer =  (
+    "We did not find official information about "+ results.name
+    ); 
+    detailsButton.style.display = "none"
+    return answer
+} else {
+  if (results.hasEsg == false) {
+    let answer = (
+    "This website belongs to "+ results.business_name + 
+    "<br>" + results.business_name + " did not make their information public"
+    );
+    detailsButton.style.display = "block"
+    return answer
+  } else if (results.hasEsg == true) {
+    let answer = (
+    "This website belongs to " + results.business_name +
+    ":<br>\u2022 ESG risk score: "+ results.yahoo_esg + "% (" + results.yahoo_percentile +
+    ")<br>\u2022 Environmental risk: "+ results.yahoo_envrisk
+    );
+    detailsButton.style.display = "block"
+    return answer
+  }
+}
+}
+
+var detailsButton = document.getElementById('details');
+detailsButton.addEventListener('click', function() {
+  chrome.tabs.getSelected(null, function(tab) {      
+    alert('thats not ready yet');
+    });
+  }, false);
